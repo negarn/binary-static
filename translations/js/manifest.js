326e3921b340f4b50ea86e780150f8219b519b2f
negar <negar@binary.com>
2018-06-21 09:23:08 +0800

    Release to translations

--- translations/js/manifest.js
+++ translations/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"409857a7d740c8487089","1":"a2f3f34f9db21b420f14","2":"99104ed7cedddd42c154","3":"78b8e4412c3dd0ce975a"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"c797b3f7820d2daad7bf","1":"7c610f641cb39508fcb1","2":"8417d454b1941f0854bc","3":"e96ce2388fcecc063803"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
