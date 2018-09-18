1bbffb9c797ceb5deb4dd85da3b5b1646e1046d4
negar <negar@binary.com>
2018-09-18 11:57:59 +0800

    Deploy to ea

--- br_ea/js/manifest.js
+++ br_ea/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"cce43489594a97c6e015","2":"d70da2a5e105fe320f89","3":"323330b4b9ad54b82fe1"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"cce43489594a97c6e015","2":"6c6e03b862323551fb36","3":"323330b4b9ad54b82fe1"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
