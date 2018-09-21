58121ddd1b0c39966e6cc99b933bcc4cc4b09e8c
negar <negar@binary.com>
2018-09-21 17:03:36 +0800

    Deploy to gh-pages

--- js/manifest.js
+++ js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"fb12d8d8c767048c354d","1":"b6f4a8b9373c08629b59","2":"5ed8ee39d10d7331bc18","3":"c8d641cf8e0897d797ab"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"6efe5d05869509af0bc0","1":"cce43489594a97c6e015","2":"dea345894f60e29bee91","3":"96ec4c31087588f1a88b"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
